5194e56d805f6bdf61c8b74177e0ff8c349dbf89
Sai Varun Vukkem <varun.vukkem@gmail.com>
2015-12-22 17:35:53 +0000

    Updated second part of CalculatePower.java

--- Art_of_Science_and_Java/src/chapter_5/CalculatePower.java
+++ Art_of_Science_and_Java/src/chapter_5/CalculatePower.java
@@ -14,11 +14,18 @@ public class CalculatePower extends ConsoleProgram {
 		int n = readInt("n: ");
 		int k = readInt("k: ");
 		println ("The answer is " + raiseIntToPower(n, k));
-		
+		powerOfTwo();
 	}
 
 	private int raiseIntToPower(int n, int k) {
 		int answer = (int) Math.pow(n, k);
 		return answer;
+	}
+	
+	private void powerOfTwo() {
+		for (int i = 0; i <= 10; i++) {
+			int answer = (int) Math.pow(2, i);
+			println ("2 to the power of " + i + " = " + answer);
+		}
 	}
 }
