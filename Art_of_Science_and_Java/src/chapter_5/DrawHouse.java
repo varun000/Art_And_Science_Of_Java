f6c542e3678be06a7e59caf4b98a6d57f597e93d
Sai Varun Vukkem <varun.vukkem@gmail.com>
2015-12-25 15:52:28 +0000

    Added comments to DrawHouse.java

--- Art_of_Science_and_Java/src/chapter_5/DrawHouse.java
+++ Art_of_Science_and_Java/src/chapter_5/DrawHouse.java
@@ -36,12 +36,15 @@ public class DrawHouse extends GraphicsProgram {
 		drawDoor(x, y);
 	}
 	
+	/* Frame of the house */
 	private void drawHouse (double x, double y) {
 		
 		add(new GRect(x, y, HOUSE_WIDTH, HOUSE_HEIGHT));
 	}
 	
+	/* Roof of the house */
 	private void drawRoof (double x, double y) {
+		
 		double rx = getWidth() / 2;
 		double ry = getHeight() - HOUSE_HEIGHT - (2 * WINDOW_BLOCK_HEIGHT);
 		double rx1 = x + HOUSE_WIDTH;
@@ -50,7 +53,9 @@ public class DrawHouse extends GraphicsProgram {
 		add(new GLine (rx, ry, rx1, y));
 	}
 	
+	/* Door of the house */
 	private void drawDoor(double x, double y) {
+		
 		double dx = x + (HOUSE_WIDTH / 2) - (DOOR_WIDTH / 2);
 		double dy = y + HOUSE_HEIGHT - DOOR_HEIGHT;
 		
@@ -59,21 +64,25 @@ public class DrawHouse extends GraphicsProgram {
 		drawDoorKnob(dx, dy);
 	}
 	
+	/* Door knob for the door */
 	private void drawDoorKnob(double x, double y) {
+		
 		double kx = x + DOOR_WIDTH - DOOR_KNOB_INSET - (2 * DOOR_KNOB_RADIUS);
 		double ky = y + (DOOR_HEIGHT / 2);
 		
 		add(new GOval (kx, ky, 2 * DOOR_KNOB_RADIUS, 2 * DOOR_KNOB_RADIUS));
 	}
 	
+	/* Windows for the house Upper and Lower windows */
 	private void drawWindows(double x, double y) {
+		
 		double ubx = x + (HOUSE_WIDTH / 6);
 		double uby = y + (DOOR_HEIGHT / 4);
 		double ubx1 = x + HOUSE_WIDTH - (HOUSE_WIDTH / 6) - (2 * WINDOW_BLOCK_WIDTH);
+		
 		double lbx = x + (HOUSE_WIDTH / 8);
 		double lby = y + HOUSE_HEIGHT - DOOR_HEIGHT + (DOOR_HEIGHT / 10);
 		double lbx1 = x + HOUSE_WIDTH - (HOUSE_WIDTH / 8) - (3 * WINDOW_BLOCK_WIDTH);
-		
 		
 		drawUpperWindows(ubx, uby);
 		drawUpperWindows(ubx1, uby);
@@ -81,15 +90,20 @@ public class DrawHouse extends GraphicsProgram {
 		drawLowerWindows(lbx1, lby);
 	}
 
+	/* Upper windows */
 	private void drawUpperWindows(double x, double y) {
+		
 		add(new GRect (x, y, WINDOW_BLOCK_WIDTH, WINDOW_BLOCK_HEIGHT));
 		add(new GRect (x, y + WINDOW_BLOCK_HEIGHT, WINDOW_BLOCK_WIDTH, WINDOW_BLOCK_HEIGHT));
 		add(new GRect (x + WINDOW_BLOCK_WIDTH, y, WINDOW_BLOCK_WIDTH, WINDOW_BLOCK_HEIGHT));
 		add(new GRect (x + WINDOW_BLOCK_WIDTH, y + WINDOW_BLOCK_HEIGHT, WINDOW_BLOCK_WIDTH, WINDOW_BLOCK_HEIGHT));
 	}
 	
+	/* Lower windows */
 	private void drawLowerWindows(double x, double y) {
+		
 		drawUpperWindows(x, y);
+		
 		add(new GRect (x + (2 * WINDOW_BLOCK_WIDTH), y, WINDOW_BLOCK_WIDTH, WINDOW_BLOCK_HEIGHT));
 		add(new GRect (x + (2 * WINDOW_BLOCK_WIDTH), y + WINDOW_BLOCK_HEIGHT, WINDOW_BLOCK_WIDTH, WINDOW_BLOCK_HEIGHT));
 	}
